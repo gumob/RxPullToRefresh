f176f381c0a5401902f0f4519c219fdbc51b6cb7
gumob <account@gumob.com>
2019-01-06 18:37:53 +0900

    Set UIScrollView+Rx to internal

--- Example/Helpers.swift
+++ Example/Helpers.swift
@@ -1,5 +1,5 @@
 //
-//  UIScrollView+Rx.swift
+//  Helpers.swift
 //  RxPullToRefresh
 //
 //  Created by kojirof on 2018/12/29.
@@ -12,31 +12,31 @@ import RxSwift
 import RxCocoa
 
 /** Reactive Extension for `UIScrollView`. */
-internal extension Reactive where Base: UIScrollView {
+extension Reactive where Base: UIScrollView {
     /** Reactive wrapper for `UIScrollView.contentSize` property. */
     var contentSize: ControlProperty<CGSize> {
         let source: Observable<CGSize> = self.observeWeakly(CGSize.self,
                                                             #keyPath(UIScrollView.contentSize),
                                                             options: [.initial, .new])
-                                             .filter { $0 != nil }
-                                             .map { $0! }
-                                             .distinctUntilChanged()
-                                             .takeUntil(deallocated)
+            .filter { $0 != nil }
+            .map { $0! }
+            .distinctUntilChanged()
+            .takeUntil(deallocated)
         let observer = Binder(self.base) { (scroll: UIScrollView, contentSize: CGSize) in
             scroll.contentSize = contentSize
         }
         return ControlProperty(values: source, valueSink: observer)
     }
-
+    
     /** Reactive wrapper for `UIScrollView.contentInset` property. */
     var contentInset: ControlProperty<UIEdgeInsets> {
         let source: Observable<UIEdgeInsets> = self.observeWeakly(UIEdgeInsets.self,
                                                                   #keyPath(UIScrollView.contentInset),
                                                                   options: [.initial, .new])
-                                                   .filter { $0 != nil }
-                                                   .map { $0! }
-                                                   .distinctUntilChanged()
-                                                   .takeUntil(deallocated)
+            .filter { $0 != nil }
+            .map { $0! }
+            .distinctUntilChanged()
+            .takeUntil(deallocated)
         let observer = Binder(self.base) { (scroll: UIScrollView, contentInset: UIEdgeInsets) in
             scroll.contentInset = contentInset
         }
